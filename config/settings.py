4b2984d09163aa0c4bc27339c2056395a3951ce2
Artur <kokivirivoki@mail.ru>
2024-05-20 23:36:23 +0500

    Попытка подключения к БД

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'Tashbulatov_A_I',
+        'USER': 'postgres',
+        'PASSWORD': '1234567890',
+
     }
 }
+# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'HOST': 'localhost',
+#      'NAME': 'Tashbulatov_A_I',
+#      'USER': 'postgres',
+#     'PASSWORD': '1234567890',
 
 AUTH_PASSWORD_VALIDATORS = [
     {
