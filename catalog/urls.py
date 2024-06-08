49efffdba8fc13a46e8dc794856d733b4ac4a91e
Artur <kokivirivoki@mail.ru>
2024-06-08 22:13:53 +0500

    H_W_21_1 Create

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, products_detail
+from catalog.views import home, ContactsView, ProductsListview, ProductsDetailview
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     # path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('', products_list, name='products_list'),
-    path('products/<int:pk>/', products_detail, name='products_detail')
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('', ProductsListview.as_view(), name='products_list'),
+    path('products/<int:pk>/', ProductsDetailview.as_view(), name='products_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
@@ -7,23 +8,44 @@ def home(request):
     return render(request, template_name='home.html')
 
 
-def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} {phone} {message}')
-    return render(request, template_name='contacts.html')
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
+    extra_context = {
+        'title': 'contacts'
+    }
+
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'POST':
+            name = self.request.POST.get('name')
+            phone = self.request.POST.get('phone')
+            message = self.request.POST.get('message')
+            print(f'You have new message from {name}({phone}): {message}')
+        return super().get_context_data(**kwargs)
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
+# def contacts(request):
+#    if request.method == 'POST':
+#        name = request.POST.get('name')
+#        phone = request.POST.get('phone')
+#        message = request.POST.get('message')
+#        print(f'{name} {phone} {message}')
+#    return render(request, template_name='contacts.html')
 
-    return render(request, 'product_list.html', context)
+
+class ProductsListview(ListView):
+    model = Product
 
 
-def products_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'products_detail.html', context)
+# def products_list(request):
+#    products = Product.objects.all()
+#    context = {"products": products}
+#    return render(request, 'product_list.html', context)
+
+
+class ProductsDetailview(DetailView):
+    model = Product
+
+# def products_detail(request, pk):
+#    product = get_object_or_404(Product, pk=pk)
+#    context = {"product": product}
+#    return render(request, 'product_detail.html', context)
